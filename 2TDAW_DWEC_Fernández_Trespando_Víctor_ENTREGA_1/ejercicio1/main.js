2593a26dd576ac58643e036fa16c42b585828c73
victrespy <victrespy@gmail.com>
2025-09-24 13:03:50 +0200

    sfgn

--- 2TDAW_DWEC_Fernández_Trespando_Víctor_ENTREGA_1/ejercicio1/main.js
+++ 2TDAW_DWEC_Fernández_Trespando_Víctor_ENTREGA_1/ejercicio1/main.js
@@ -1,9 +1,13 @@
-function ej1(){
+function ejercicio1(){
     //Pedir al usuario un numero y comprobar si es natural
     do{
-        let check = true;
+        var check = true;
         let num = prompt("Introduce un numero natural: ");
-        if(typeof num == 'number' && Number.isInteger(num) && num >= 0){
+        if(num == null){
+            return;
+        }
+        num = Number(num);
+        if(typeof num == 'number' && Number.isInteger(num) && num > 0){
             check = false;
         } else {
             alert("Introduce un dato valido");
