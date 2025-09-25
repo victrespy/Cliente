1c4d215d5f2a4bef8c93e341efa292008dace62a
victrespy <victrespy@gmail.com>
2025-09-25 11:45:55 +0200

    fin ejercicio 3

--- 2TDAW_DWEC_Fernández_Trespando_Víctor_ENTREGA_1/ejercicio3/main.js
+++ 2TDAW_DWEC_Fernández_Trespando_Víctor_ENTREGA_1/ejercicio3/main.js
@@ -1,3 +1,18 @@
 function ejercicio(){
     
+    let presupuesto = prompt("Introduce el presupuesto total de la obra: ");
+    if(presupuesto <= 0){
+        document.writeln("<h1>Error: Presupuesto no valido</h1>");
+    } else {
+        let materiales = presupuesto * 0.5;
+        let licencias = presupuesto * 0.3;
+        let mano = presupuesto * 0.2;
+
+        document.writeln("<h1>Presupuesto de la obra</h1>");
+        document.writeln(`<ul>
+                            <li>Materiales: ${materiales}€</li>
+                            <li>Licencias de obra: ${licencias}€</li>
+                            <li>Mano de obra: ${mano}€</li>
+                        </ul>`);
+    }
 }
