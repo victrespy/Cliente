17396be0bd912fb190611be8e7fdf10d06b3498a
victrespy <victrespy@gmail.com>
2025-09-25 11:10:18 +0200

    Fin ejercicio 1

--- 2TDAW_DWEC_Fernández_Trespando_Víctor_ENTREGA_1/ejercicio1/main.js
+++ 2TDAW_DWEC_Fernández_Trespando_Víctor_ENTREGA_1/ejercicio1/main.js
@@ -2,7 +2,7 @@ function ejercicio1(){
     //Pedir al usuario un numero y comprobar si es natural
     do{
         var check = true;
-        let num = prompt("Introduce un numero natural: ");
+        var num = prompt("Introduce un numero natural: ");
         if(num == null){
             return;
         }
@@ -13,4 +13,29 @@ function ejercicio1(){
             alert("Introduce un dato valido");
         }
     } while (check);
+
+    //Cuando se introduce un numero natural, realizar las operaciones
+    //Calcular los divisores
+    let divisores = [];
+    for(let i = 1; i <= num; i++){
+        if(num % i == 0){
+            divisores.push(i);
+        }
+    }
+    alert("Los divisores de " + num + " son: " + divisores.join(", "));
+
+    //Calcular la suma de los cuadrados de los divisores
+    let sumaCuadrados = 0;
+    for(let i = 0; i < divisores.length; i++){
+        sumaCuadrados += Math.pow(divisores[i], 2);
+    }
+    alert("La suma de los cuadrados de los divisores de " + num + " es: " + sumaCuadrados);
+
+    //Decir si esa suma es un cuadrado o no
+    let raiz = Math.sqrt(sumaCuadrados);
+    if(Number.isInteger(raiz)){
+        alert("La suma de los cuadrados de los divisores de " + num + " es un cuadrado");
+    } else {
+        alert("La suma de los cuadrados de los divisores de " + num + " no es un cuadrado");
+    }
 }
